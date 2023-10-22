c8e8e4326fac1169ca17fe14c812761435eec889
ahmetovali <ahmetovali1903@gmail.com>
2023-10-23 01:04:20 +0300

    Search

--- food-ordering/components/layout/Header.jsx
+++ food-ordering/components/layout/Header.jsx
@@ -1,13 +1,15 @@
-import Logo from "../ui/Logo"
+import { useState } from "react";
+import Logo from "../ui/Logo";
+import {FaUserAlt, FaShoppingCart, FaSearch, FaAccessibleIcon} from "react-icons/fa";
+import Search from "../ui/Search";
 
 
 const Header = () => {
+    const [isSearchModal,setIsSearchModal] = useState(false)
   return (
     <div className="h-[5.5rem] bg-secondary">
         <div className="container mx-auto text-white flex justify-between items-center h-full">
-        <div>
-            <Logo />
-        </div>
+        <Logo />
         <nav>
            <ul className="flex gap-x-2">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
@@ -24,10 +26,25 @@ const Header = () => {
             </li>
            </ul>
         </nav>
-        <div></div>
+        <div className="flex gap-x-4 items-center">
+            <a href="#">
+                <FaUserAlt className="hover:text-primary transition-all"/>
+            </a>
+            <a href="#">
+                <FaShoppingCart className="hover:text-primary transition-all"/>
+            </a>
+            <button onClick={() => setIsSearchModal(true)}>
+                <FaSearch className="hover:text-primary transition-all"/>
+            </button>
+            <a href="#">
+                <button className="btn-primary">Order Online</button>
+            </a>
         </div>
-    </div>
-    
+        </div>
+        {isSearchModal && (
+            <Search setIsSearchModal={setIsSearchModal}/>    
+        )}
+    </div>    
   )
 }
 
