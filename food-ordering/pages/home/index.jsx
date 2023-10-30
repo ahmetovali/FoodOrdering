b5f38c37bb67c0482d0681905e9974e8e046d780
ahmetovali <ahmetovali1903@gmail.com>
2023-10-30 21:23:43 +0300

    Customers

--- food-ordering/pages/home/index.jsx
+++ food-ordering/pages/home/index.jsx
@@ -2,18 +2,20 @@ import About from "@/components/About";
 import Campaigns from "@/components/Campaigns";
 import Carousel from "@/components/Carousel";
 import Reservation from "@/components/Reservation";
+import Customers from "../../components/customers/Customers";
 import MenuWrapper from "@/components/product/MenuWrapper";
 import React from "react";
 
 const Index = () => {
   return (
-    <div>
+    <React.Fragment>
       <Carousel/>
       <Campaigns/>
       <MenuWrapper/>
       <About/>
       <Reservation />
-    </div>
+      <Customers />
+    </React.Fragment>
   );
 };
 
